137748e4e4e0b5fa70b5ecd574ffa3e797c82f44
Daniil Dubrovsky <stillcxld@icloud.com>
2022-12-21 00:19:16 +0400

    first commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,12 @@
+public class BonusMilesService {
+
+    public int calculate(int price) {
+        int sum = 20;
+        int result = price / sum;
+        return result;
+
+    }
+}
+
+
+
---
+++ src/java4.java
@@ -0,0 +1,8 @@
+public class java4 {
+    public static void main(String[] args) {
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
+    }
+}
